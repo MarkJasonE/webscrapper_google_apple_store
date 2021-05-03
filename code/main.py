35073f98ec3e37daa8b0190f3954bc397dd84140
JasonEgipto <jasonegipto@gmail.com>
2021-05-03 11:57:16 +0300

    Added emoji removal. Working on tokenizing text for sentiment analysis and clustering

--- code/main.py
+++ code/main.py
@@ -1,67 +1,28 @@
-import os
-import time
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import pandas as pd
+from google_play_scraper import app, Sort, reviews_all, reviews
+from app_store_scraper  import AppStore
 
-url = "https://play.google.com/store/apps/details?id=com.covalent.kippo&hl=en_US&gl=US&showAllReviews=true"
+DATA_DIR_PATH = "data/"
 
-chromebrowser = r"C:\Users\Jason\Downloads\chromedriver\chromedriver.exe"
-os.environ["webdriver.chrome.driver"] = chromebrowser
-browser = webdriver.Chrome(chromebrowser)
-browser.get(url)
+#Google play store
+def google_to_csv(app_name=None, data_src="google"):
+  results = reviews_all(app_name)
+  df = pd.DataFrame(results)
+  df.to_csv(DATA_DIR_PATH + data_src + "_reviews.csv", index=False, encoding="utf-8")
+
+#Return reviews for us and canada
+google_to_csv(app_name="com.covalent.kippo")
 
 
-#Because the site loads slowly, we need to wait 5 seconds until we can get the full page source
-time.sleep(5)
-html = browser.page_source
-soup = BeautifulSoup(html, "lxml")
+#Apple app store
+def app_to_csv(country=None, app_name=None, data_src="apple"):
+  app = AppStore(country=country, app_name=app_name)
+  app.review()
+  df = pd.DataFrame(app.reviews)#All of the reviews
+  df.to_csv(DATA_DIR_PATH + data_src + "_reviews_" + country + ".csv", index=False, encoding="utf-8")
 
-#Scroll down until you get all the reviews
-SCROLL_PAUSE_TIME = 1
+#US
+app_to_csv(country="us", app_name="kippo-dating-app-for-gamers")
 
-#Get scroll height
-last_height = browser.execute_script("return document.body.scrollHeight")
-
-while True:
-  #Scroll down
-  browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-  #Wait until finished loading more reviews
-  time.sleep(SCROLL_PAUSE_TIME)
-
-  #Calculate new scroll height and compare to last scroll height
-  new_height = browser.execute_script("return document.body.scrollHeight")
-  if new_height == last_height:
-    print("breaked while loop")
-    break
-  last_height = new_height
-
-#Put this 2 after it has finished scraping
-#browser.close()
-#browser.quit()
-
-# after scrolling a few times a div with the "role=button class='U26fgb O0WRkf oG5Srb C0oVfc n9lfJ M9Bg4d'". Check if that button exist, then click it and continue scrolling down
-
-"""
-reviews_boxes = soup.find_all("div", class_="d15Mdf")
-for reviews_box in reviews_boxes:
-  rev_name = reviews_box.find("span", class_="X43Kjb").text
-  rev_pub_date = reviews_box.find("span", class_= "p2TkOb").text
-  rev_stars = int(reviews_box.find("div", class_="pf5lIe").next_element["aria-label"].split()[1]) #The element that we want doesnt have a class/id. Then from the returned string, we only take the number
-  rev_likes = int(reviews_box.find("div", class_="jUL89d y92BAb").text)
-
-  if reviews_box.find("button", class_="LkLjZd ScJHi OzU4dc"):
-    #Long comment
-    rev_comment = reviews_box.find("span", {"jsname":"fbQN7e"}).text
-  else:
-    #Short comment
-    rev_comment = reviews_box.find("span", {"jsname":"bN97Pc"}).text
-
-  print(f"Name: {rev_name}")
-  print(f"Date posted: {rev_pub_date}")
-  print(f"Stars: {rev_stars}")
-  print(f"Likes: {rev_likes}")
-  print(f"Comments: {rev_comment}")
-
-"""
+#CA
+app_to_csv(country="ca", app_name="kippo-dating-app-for-gamers")
---
+++ code/utils.py
@@ -0,0 +1,71 @@
+import emoji
+import re
+import pandas as pd
+import numpy as np
+
+
+"""
+Table of contents:
+Section A: Data Wrangling:
+  1 - Functions for columns/rows manipulations
+  2 - Functions for string manipulations
+  3 - Functions for time manipulations
+
+Section B: Data Visualization:
+  1 - Functions for pie charts
+"""
+
+#~~~~~~~~# Section A: DATA WRANGLING #~~~~~~~~#
+
+###### A-1 COLUMNS/ROWS MANIPULATION ######
+
+#A-1.1 Dropping columns
+def drop_col(df, cols):
+  df.drop(cols, axis=1, inplace=True)
+
+###### END A-1 COLUMN/ROWS MANIPULATION ######
+
+
+###### A-2 STRING MANIPULATION ######
+
+#A-2.1 Clean text from additional white space and line breaks "\n"
+def clean_txt(df, col):
+  df[col].replace(r"\s+|\\n", " ", regex=True, inplace=True)
+
+#A-2.2 Clean string from emojis
+def remove_emoji(text):
+  new_txt = emoji.get_emoji_regexp().sub(r"", text)
+  return new_txt
+
+#A-2.3 Clean time string from upper/lower case and white spaces before converting
+def clean_time(df, col):
+  df[col] = df[col].str.replace(r"[a-zA-Z ]|\s+", " ", regex=True)
+
+
+
+###### END A-2 STRING MANIPULATION ######
+
+
+###### A-3 TIME MANIPULATION ######
+
+#A-3.1 Convert time into date time after cleaning
+def convert_to_datetime(df, col):
+  df[col] = pd.to_datetime(df[col])
+
+###### END A-3 TIME MANIPULATION ######
+
+#~~~~~~~~# End Of Section A: DATA WRANGLING #~~~~~~~~#
+
+
+#~~~~~~~~# Section B: DATA VISUALIZATION #~~~~~~~~#
+
+###### B-1 PIE CHARTS######
+
+#B-1.1 Show percentage and its total number of N from a specified population
+def show_pie_val_pct(pct, total_n):
+  absolute = int(np.round(pct/100 * np.sum(total_n)))
+  return "{:.1f}%\nN={}".format(pct, absolute)
+
+###### END B-1 PIE CHARTS######
+
+#~~~~~~~~# End Of Section B: DATA VISUALIZATION #~~~~~~~~#
